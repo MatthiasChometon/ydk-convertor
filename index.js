53d722aed8950570d690ae313431a45f14fc1190
MatthiasChometon <matthiaschometon787@gmail.com>
2021-11-15 13:59:26 +0100

    feat: add size option

--- index.js
+++ index.js
@@ -23,25 +23,19 @@ async function downloadDecksByPaths(files) {
 
 async function downloadDeckByPath(file) {
     const path = `${file.replace('.ydk', '')}`
-
-    const [arr] = await Promise.all([
-        getYdkIds(file),
-        createDirectories(path)
-    ])
+    const arr = await getYdkIds(file)
 
     downloadPicturesById(arr, path)
-}
-
-async function createDirectories(path) {
-    mkdir(path, { recursive: true })
-    console.log('create directory')
 }
 
 async function getYdkIds(file) {
     const ydkPath = await readFile(`${process.argv[3]}/${file}`)
     const arr = ydkPath.toString().split('\n')
 
+    let breakYdk = false
     const ydks = arr.reduce((ydkArr, currentPath) => {
+        if (process.argv[4] === "--noside" && currentPath === '!side\r') breakYdk = true
+        if (breakYdk === true) return ydkArr
         currentPath = parseInt(currentPath)
         if (isNaN(currentPath)) return ydkArr
         const newCardNumber = ydkArr[currentPath] === undefined ? 1 : ydkArr[currentPath] + 1
@@ -62,10 +56,12 @@ async function downloadPicturesById(ids, path) {
 
 async function downloadPictureById(id, number, directoryPath) {
     const json = await axios.get(`https://db.ygoprodeck.com/api_internal/v7/cardinfo.php?id=${id}`)
-        .catch(err => console.log(err))
+        .catch(err => err)
+
+    if (json.status === undefined) return
     const pictureUrl = json.data.data[0].card_images[0].image_url
     const name = json.data.data[0].name
-    const picturePath = `${directoryPath}/${number}_${name.replace(/[^a-zA-Z0-9]/g, '')}.jpg`
+    const picturePath = `decks/${directoryPath}/${number}_${name.replace(/[^a-zA-Z0-9]/g, '')}.jpg`
 
     switch (process.argv[2]) {
         case '-d':
@@ -75,14 +71,15 @@ async function downloadPictureById(id, number, directoryPath) {
             })
             break
         case '-c':
+            let error = false
             const picture = await Jimp.read(pictureUrl)
-                .catch(err => {
-                    console.error(err)
-                })
-            picture
-                .contain(picture.bitmap.width * 1.2, picture.bitmap.height)
-                .contain(picture.bitmap.width, picture.bitmap.height * 1.3)
-                .write(picturePath)
+                .catch(() => error = true)
+            if (!error) {
+                picture
+                    .contain(picture.bitmap.width * 1.2, picture.bitmap.height)
+                    .contain(picture.bitmap.width, picture.bitmap.height * 1.3)
+                    .write(picturePath)
+            }
             break
     }
 
