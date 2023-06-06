96bec896354f1934407b1ffde8440e84d4c9eb7d
MatthiasChometon <matthiaschometon787@gmail.com>
2023-06-06 22:10:27 +0200

    fix: download all pictures

--- index.js
+++ index.js
@@ -1,91 +1,96 @@
-const https = require('https')
 const fs = require('fs')
 const util = require('util')
 const axios = require('axios')
 var Jimp = require('jimp')
 
 const readFile = util.promisify(fs.readFile)
-const mkdir = util.promisify(fs.mkdir)
 
-async function getfile() {
+let dl = 0
+let toDl = 0
+
+function getfile() {
     fs.readdir(process.argv[3], async function (err, files) {
         if (err) throw new Error('ydk files not founded')
-        console.log('ydk files founded')
-        downloadDecksByPaths(files)
+        await downloadDecksByPaths(files)
     })
 }
 
 async function downloadDecksByPaths(files) {
     await Promise.all(
-        files.map(async (file) => downloadDeckByPath(file))
+        files.map(async (file) => await downloadDeckByPath(file))
     )
 }
 
 async function downloadDeckByPath(file) {
     const path = `${file.replace('.ydk', '')}`
+    const arr = await getYdkIds(file)
 
-    const [arr] = await Promise.all([
-        getYdkIds(file),
-        createDirectories(path)
-    ])
-
-    downloadPicturesById(arr, path)
-}
-
-async function createDirectories(path) {
-    mkdir(path, { recursive: true })
-    console.log('create directory')
+    await downloadPicturesById(arr, path)
 }
 
 async function getYdkIds(file) {
     const ydkPath = await readFile(`${process.argv[3]}/${file}`)
     const arr = ydkPath.toString().split('\n')
 
+    let breakYdk = false
     const ydks = arr.reduce((ydkArr, currentPath) => {
+        if (process.argv[4] === "--noside" && currentPath === '!side\r') breakYdk = true
+        if (breakYdk === true) return ydkArr
         currentPath = parseInt(currentPath)
         if (isNaN(currentPath)) return ydkArr
         const newCardNumber = ydkArr[currentPath] === undefined ? 1 : ydkArr[currentPath] + 1
         return { ...ydkArr, [currentPath]: newCardNumber }
     }, {})
 
-    console.log('card ids founded')
+    toDl += Object.keys(ydks).length
     return ydks
 }
 
 async function downloadPicturesById(ids, path) {
     await Promise.all(
-        Object.keys(ids).map(id => {
-            downloadPictureById(id, ids[id], path)
+        Object.keys(ids).map(async id => {
+            await downloadPictureById(id, ids[id], path)
         })
     )
 }
 
+function delay(ms) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+async function downloadApiPictureUrl (id) {
+    const apiPictureUrl = `https://db.ygoprodeck.com/api_internal/v7/cardinfo.php?id=${id}`
+    return await axios.get(apiPictureUrl)
+        .catch(async () => {
+            await delay(1000)
+            return await downloadApiPictureUrl(id)
+        })
+}
+
 async function downloadPictureById(id, number, directoryPath) {
-    const json = await axios.get(`https://db.ygoprodeck.com/api_internal/v7/cardinfo.php?id=${id}`)
-        .catch(err => console.log(err))
+    const json = await downloadApiPictureUrl(id)
     const pictureUrl = json.data.data[0].card_images[0].image_url
     const name = json.data.data[0].name
-    const picturePath = `${directoryPath}/${number}_${name.replace(/[^a-zA-Z0-9]/g, '')}.jpg`
+    const picturePath = `${process.argv[3]}/decks/${directoryPath}/${number}_${name.replace(/[^a-zA-Z0-9]/g, '')}.jpg`
+    const picture = await Jimp.read(pictureUrl)
 
     switch (process.argv[2]) {
         case '-d':
-            https.get(pictureUrl, (picture) => {
-                const file = fs.createWriteStream(picturePath)
-                picture.pipe(file)
-            })
+            picture.write(picturePath)
             break
         case '-c':
-            const picture = await Jimp.read(pictureUrl)
-                .catch(err => {
-                    console.error(err)
-                })
             picture
-                .contain(picture.bitmap.width * 1.2, picture.bitmap.height)
-                .contain(picture.bitmap.width, picture.bitmap.height * 1.3)
+                .resize(421, 614 * 1.07)
+                .contain(picture.bitmap.width * 1.11, picture.bitmap.height)
+                .contain(picture.bitmap.width, picture.bitmap.height * 1.28)
                 .write(picturePath)
             break
     }
 
+    dl++
+    console.clear()
+    console.log(`downloaded ${dl} / ${toDl} ...`)
+    if (dl === toDl) console.log('all cards have been downloaded')
     console.log(picturePath)
 }
 
